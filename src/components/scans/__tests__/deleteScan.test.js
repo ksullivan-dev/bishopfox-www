170833f1a290a5681341685dd626835cd1237d5c
Kyle Sullivan <ksullivan@roundsphere.com>
2019-10-15 10:15:24 -0400

    Remove unused tests

--- src/components/scans/__tests__/deleteScan.test.js
+++ src/components/scans/__tests__/deleteScan.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, fireEvent, waitForElement } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import mockFetchPromise from 'utilities/mockFetchPromise';
 import DeleteScan from 'components/scans/deleteScan';
@@ -32,11 +32,7 @@ describe('Delete Scan tests', () => {
       redirect: true
     };
     const result = () => mockFetchPromise({ fakeResponse });
-    // TODO why do I have to do it this way instead of with spyOn?
     global.fetch = jest.fn().mockImplementation(() => result());
-    // jest.spyOn(global, 'fetch').mockImplementationOnce(() => {
-    //     return mockFetchPromise;
-    // });
     const button = getByText(/delete scan/i);
 
     // Event to show confirm modal
@@ -54,66 +50,4 @@ describe('Delete Scan tests', () => {
     expect(response.redirect).toBe(true);
     expect(response.redirect_url).toBe('www.redirecturl.com');
   });
-
-  // it('renders the ErrorHandler component on error with an array of errors', async () => {
-  //   const { getByText, getByTestId } = render(<DeleteScan scan={{ id: 2 }} />);
-  //   // Errors can either be an object with a message prop or an array
-  //   const fakeResponse = ['input cannot be empty'];
-  //   const result = () => mockFetchPromise({ fakeResponse, err: true });
-  //   global.fetch = jest.fn().mockImplementation(() => result());
-  //   const button = getByText(/delete order/i);
-
-  //   // Event to show confirm modal
-  //   fireEvent.click(button);
-  //   const actual_delete = getByTestId('test_actually-delete');
-
-  //   // Event to actually delete the order
-  //   fireEvent.click(actual_delete);
-  //   const errorhandler = await waitForElement(() =>
-  //     getByText(/input cannot be empty/i)
-  //   );
-  //   expect(errorhandler).toBeVisible();
-  // });
-
-  //   it('renders the ErrorHandler component on error with an empty array', async () => {
-  //     const { getByText, getByTestId } = render(<Delete id="12" />);
-  //     // Errors can either be an object with a message prop or an array
-  //     const fakeResponse = [];
-  //     const result = () => mockFetchPromise({ fakeResponse, err: true });
-  //     global.fetch = jest.fn().mockImplementation(() => result());
-  //     const button = getByText(/delete order/i);
-
-  //     // Event to show confirm modal
-  //     fireEvent.click(button);
-  //     const actual_delete = getByTestId('test_actually-delete');
-
-  //     // Event to actually delete the order
-  //     fireEvent.click(actual_delete);
-
-  //     const errorhandler = await waitForElement(() =>
-  //       getByText(/something went wrong/i)
-  //     );
-  //     expect(errorhandler).toBeVisible();
-  //   });
-
-  //   it('renders the ErrorHandler component on error with errors as an object', async () => {
-  //     const { getByText, getByTestId } = render(<DeleteOrder id="12" />);
-  //     // Errors can either be an object with a message prop or an array
-  //     const fakeResponse = { message: 'input cannot be empty' };
-  //     const result = () => mockFetchPromise({ fakeResponse, err: true });
-  //     global.fetch = jest.fn().mockImplementation(() => result());
-  //     const button = getByText(/delete order/i);
-
-  //     // Event to show confirm modal
-  //     fireEvent.click(button);
-  //     const actual_delete = getByTestId('test_actually-delete');
-
-  //     // Event to actually delete the order
-  //     fireEvent.click(actual_delete);
-
-  //     const errorhandler = await waitForElement(() =>
-  //       getByText(/input cannot be empty/i)
-  //     );
-  //     expect(errorhandler).toBeVisible();
-  //   });
 });
