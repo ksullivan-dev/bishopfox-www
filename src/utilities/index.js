3ae1cac2c67f27c1944acade1b49b064f6006fc0
Kyle Sullivan <ksullivan@roundsphere.com>
2019-10-15 12:02:37 -0400

    Remove truncate from utils

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,14 +2,6 @@ import capitalize from './capitalize';
 import callMeDanger from './callMeDanger';
 import jsonToFormData from './jsonToFormData';
 import readCookie from './readCookie';
-import truncate from './truncate';
 import underscorer from './underscorer';
 
-export {
-  capitalize,
-  callMeDanger,
-  jsonToFormData,
-  readCookie,
-  truncate,
-  underscorer
-};
+export { capitalize, callMeDanger, jsonToFormData, readCookie, underscorer };
--- src/utilities/truncate.js
+++
@@ -1,7 +0,0 @@
-const truncate = (str, len, useElipsis = true) => {
-  const elip = useElipsis ? '...' : '';
-  return typeof str === 'string' && str.length > len
-    ? str.slice(0, len) + elip
-    : str;
-};
-export default truncate;
