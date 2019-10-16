a0c8bb26c995d83de0ee5826f748dea3e70d5b09
Kyle Sullivan <ksullivan@roundsphere.com>
2019-10-15 21:00:50 -0400

    Add jest-fetch-mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect';
 
+global.fetch = require('jest-fetch-mock');
+
 // export * from '@testing-library/react';
 // export { customRender as render, mockFetchPromise };
 // export { mockFetchPromise };
